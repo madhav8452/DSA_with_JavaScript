66860974b20b29b788ba787e4635f5ce91ec8cf9
madhav8452 <madhavrathod019@gmail.com>
2024-11-22 19:08:01 +0530

    Exam

--- Day 7 Exam/Q5.js
+++ Day 7 Exam/Q5.js
@@ -5,8 +5,8 @@ let ans = []
 
 for(let i = 0; i < arr.length; i++){
     if(arr[i] % 2 == 0){
-        let square = arr[i] * 2
-        ans.push(square)
+        let Doubled = arr[i] * 2
+        ans.push(Doubled)
     }
     else{
         let half = arr[i] / 2
